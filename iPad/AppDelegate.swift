576e6ae19a1bad7ace802d830b7f0a293e5fa30e
Anson <zenghaojim33@qq.com>
2022-11-04 10:49:47 +0800

    no message

--- iPad/AppDelegate.swift
+++ iPad/AppDelegate.swift
@@ -10,12 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
+    var anotherWindow: UIWindow!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
+    
+
 
     // MARK: UISceneSession Lifecycle
 
@@ -31,6 +33,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    
 
+}
+
+extension UIWindowScene {
+    static var focused: UIWindowScene? {
+        return UIApplication.shared.connectedScenes
+            .first { $0.activationState == .foregroundActive && $0 is UIWindowScene } as? UIWindowScene
+    }
 }
 
--- iPad/ViewController.swift
+++ iPad/ViewController.swift
@@ -7,13 +7,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+var popupWindow: UIWindow?
+
+class FadeWindow: UIWindow {
+    override func makeKeyAndVisible() {
+        super.makeKeyAndVisible()
+        self.alpha = 0
+        UIView.animate(withDuration: 0.24) {
+            self.alpha = 1
+        }
+    }
+}
+
+
+class ViewController1: UIViewController {
+
+    var gradient: CAGradientLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.gradient = CAGradientLayer()
+
+        gradient?.frame = view.bounds
+        gradient?.colors = [UIColor.green.cgColor, UIColor.yellow.cgColor]
+
+        view.layer.insertSublayer(gradient!, at: 0)
     }
+    
+    @IBAction func show(_ sender: Any) {
+        let windowScene = UIApplication.shared.connectedScenes.first
+        if let windowScene = windowScene as? UIWindowScene {
+            popupWindow = FadeWindow(windowScene: windowScene)
+            popupWindow?.frame = UIScreen.main.bounds
+            let nav = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "nav")
+            nav.view.frame = UIScreen.main.bounds.inset(by: .init(top: 150, left: 150, bottom: 150, right: 150))
+            let root = UIViewController()
+            root.view?.backgroundColor = .black.withAlphaComponent(0.5)
+            let options = UIWindow.TransitionOptions(direction: .fade, style: .easeInOut)
+            options.background = UIWindow.TransitionOptions.Background.solidColor(UIColor.black)
+            options.duration = 0.25
 
-
+            popupWindow?.set(rootViewController: root, options: options, nil)
+            root.addChild(nav)
+            root.view.addSubview(nav.view)
+            nav.didMove(toParent: root)
+            popupWindow?.windowLevel = UIWindow.Level.statusBar + 1
+            popupWindow?.makeKeyAndVisible()
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        gradient?.frame = view.bounds
+    }
+    
 }
 
+class ViewController: UIViewController {
+    
+}
